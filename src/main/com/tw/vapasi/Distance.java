08b3874db8adb4b63a664dff811077c8231ff5db
Madhupriya123 <madhunats@gmail.com>
2019-08-31 15:46:23 +0530

    [Madhu] Add . Test cases and implementations of 9 different combinations of cm,m,km conversions.

--- src/main/com/tw/vapasi/Distance.java
+++ src/main/com/tw/vapasi/Distance.java
@@ -1,4 +1,63 @@
 package com.tw.vapasi;
 
-public class Distance {
+import java.util.Objects;
+
+//understands length with different units
+class Distance {
+
+   private static final double ONE_METER_IN_CENTIMETER = 100;
+   private static final double ONE_KILOMETER_IN_CENTIMETER = 100000;
+   private final double value;
+   private final Unit unit;
+
+    Distance(double value, Unit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    static Distance centimeter(int magnitude) {
+        return new Distance(magnitude, Unit.CM);
+    }
+    static Distance meter(int magnitude) {
+        return new Distance(magnitude, Unit.M);
+    }
+
+        static Distance kilometer(int magnitude) {
+            return new Distance(magnitude, Unit.KM);
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) return true;
+        if (otherObject == null || getClass() != otherObject.getClass()) return false;
+       Distance other = (Distance)otherObject;
+        return this.converttoCm() == other.converttoCm();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.unit.hashCode() + this.value);
+    }
+
+    double converttoCm()
+    {
+        double result = 0;
+        switch (this.unit) {
+            case M:
+              return   this.value * ONE_METER_IN_CENTIMETER;
+            case KM:
+                return this.value * ONE_KILOMETER_IN_CENTIMETER;
+        }
+        return this.value;
+        }
+
+
+    enum Unit{
+        KM,
+        CM,
+        M
+    }
+
 }
+
+
