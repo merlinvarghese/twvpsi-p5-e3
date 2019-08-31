64d56dc9a64558c5fb88ae812bdf2ea3dee653aa
Madhupriya123 <madhunats@gmail.com>
2019-08-31 15:50:47 +0530

    [Madhu] Add . Formatting

--- src/main/com/tw/vapasi/Distance.java
+++ src/main/com/tw/vapasi/Distance.java
@@ -5,10 +5,10 @@ import java.util.Objects;
 //understands length with different units
 class Distance {
 
-   private static final double ONE_METER_IN_CENTIMETER = 100;
-   private static final double ONE_KILOMETER_IN_CENTIMETER = 100000;
-   private final double value;
-   private final Unit unit;
+    private static final double ONE_METER_IN_CENTIMETER = 100;
+    private static final double ONE_KILOMETER_IN_CENTIMETER = 100000;
+    private final double value;
+    private final Unit unit;
 
     Distance(double value, Unit unit) {
         this.value = value;
@@ -18,19 +18,31 @@ class Distance {
     static Distance centimeter(int magnitude) {
         return new Distance(magnitude, Unit.CM);
     }
+
     static Distance meter(int magnitude) {
         return new Distance(magnitude, Unit.M);
     }
 
-        static Distance kilometer(int magnitude) {
-            return new Distance(magnitude, Unit.KM);
+    static Distance kilometer(int magnitude) {
+        return new Distance(magnitude, Unit.KM);
+    }
+
+
+    private double converttoCm() {
+        switch (this.unit) {
+            case M:
+                return this.value * ONE_METER_IN_CENTIMETER;
+            case KM:
+                return this.value * ONE_KILOMETER_IN_CENTIMETER;
+        }
+        return this.value;
     }
 
     @Override
     public boolean equals(Object otherObject) {
         if (this == otherObject) return true;
         if (otherObject == null || getClass() != otherObject.getClass()) return false;
-       Distance other = (Distance)otherObject;
+        Distance other = (Distance) otherObject;
         return this.converttoCm() == other.converttoCm();
     }
 
@@ -39,20 +51,7 @@ class Distance {
         return Objects.hashCode(this.unit.hashCode() + this.value);
     }
 
-    double converttoCm()
-    {
-        double result = 0;
-        switch (this.unit) {
-            case M:
-              return   this.value * ONE_METER_IN_CENTIMETER;
-            case KM:
-                return this.value * ONE_KILOMETER_IN_CENTIMETER;
-        }
-        return this.value;
-        }
-
-
-    enum Unit{
+    enum Unit {
         KM,
         CM,
         M
